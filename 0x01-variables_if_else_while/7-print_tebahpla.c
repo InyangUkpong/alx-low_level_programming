003f48d873c42bd53d5aeb53a091fb0fbf4205d7
InyangUkpong <inyangweb@gmail.com>
2022-06-03 12:40:45 -0700

    7 print-reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet at reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'z'; b >= 'a'; b--)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	ruturn (0);
+}
