a2153d9eb5bf93ee9a8996a737da53d02aa86943
InyangUkpong <inyangweb@gmail.com>
2022-06-03 13:41:56 -0700

    9-print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a series of nums with commas
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		putchar(b);
+		if (b != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
