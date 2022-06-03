9ff7d31e206b2cede60ca554856d7db588763f24
InyangUkpong <inyangweb@gmail.com>
2022-06-03 10:52:28 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabeth
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char q;
+	for (q = 'a'; c <= 'z'; q++)
+		putchar(q);
+
+	putchar('\n');
+	return (0);
+}
