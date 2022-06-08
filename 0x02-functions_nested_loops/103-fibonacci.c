8a98bba339be36946fc728ae52d4434f7ca06ae9
InyangUkpong <inyangweb@gmail.com>
2022-06-08 16:31:14 -0700

    find and prints the first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci numbers
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+
+{
+	long int f, s, sum, tot;
+
+	sum = 0;
+	f = 0;
+	s = 1;
+	tot = 0;
+
+while (sum < 400000)
+{
+	sum = f + s;
+if (sum % 2 == 0)
+{
+	tot += sum;
+}
+
+	f = s;
+	s = sum;
+}
+	printf("%ld\n", tot);
+	return (0);
+
+}
