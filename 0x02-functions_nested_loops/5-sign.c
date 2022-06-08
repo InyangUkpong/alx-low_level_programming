2a4ca612854d66fa9734210b82d3b12acb3c3a30
InyangUkpong <inyangweb@gmail.com>
2022-06-08 07:23:19 -0700

    Corrections 5 - functions that print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,12 +2,11 @@
 
 /**
  * print_sign - a function that prints the sign of a number
- * @c: the character to check
- * Return: 1 if c is lowercase else return 0
+ * @n: the character to check
+ * Return: 1 if n is lowercase else return 0
  */
 
 int print_sign(int n)
-
 {
 if (n > 0)
 {
