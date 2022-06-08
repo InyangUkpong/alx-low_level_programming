b8756883b70f570332a8db6cedac594c5a5e9d52
InyangUkpong <inyangweb@gmail.com>
2022-06-08 06:41:58 -0700

    function that prints sign of a num

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that prints the sign of a number
+ * @c: the character to check
+ * Return: 1 if c is lowercase else return 0
+ */
+
+int print_sign(in n)
+
+{
+	if (n > 0)
+	{
+		r = print_sign(75);
+		_putchar (r);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		r = print_sign(77);
+		_putchar (r);
+		return (-1);
+	}
+	else
+}
+r = print_sign(80);
+_putchar(r);
+return (0);
+}
+}
