900642a39b5922ee7c2d26980259c32191b24390
InyangUkpong <inyangweb@gmail.com>
2022-06-10 11:09:15 -0700

    pritn 10 times numbers from 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers from 0 - 14
+ * description: can only use _putchar 3 times
+ * 
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int q, r;
+
+	q = 0;
+	while (q < 10)
+	{
+		r = o;
+		while (r < 15)
+		{
+			if (r > 9)
+				_putchar(r / 10 + '0');
+
+			_putchar(r % 10 + '0');
+			r++;
+		}
+		q++;
+		_putchar('\n');
+	}
+}
