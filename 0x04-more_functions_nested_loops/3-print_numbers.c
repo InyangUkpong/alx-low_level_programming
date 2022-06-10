fe51ad7c9b10267e4561ad565c02abc10ef9216f
InyangUkpong <inyangweb@gmail.com>
2022-06-10 10:42:19 -0700

    3 print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers 
+ * print_numbers - print numbers
+ * Return: Always 0 (success) 
  */
 
 void print_numbers(void)
 {
 	int q;
+	q = 0;
 
 	for (q = 0; q < 10; q++)
 		_putchar(q + '0');
