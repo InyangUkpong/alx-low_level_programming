9b7f267ca55f8fd2b04251f4d42f033ff6155658
InyangUkpong <inyangweb@gmail.com>
2022-06-13 13:12:05 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - print reverse string
+ *
+ * @s: string
+ */
+
+void pring_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	while (len >=0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
