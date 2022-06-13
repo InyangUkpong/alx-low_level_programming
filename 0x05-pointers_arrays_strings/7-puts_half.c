e3cecd6326aa7fee0b98e24e73509815162aaf9d
InyangUkpong <inyangweb@gmail.com>
2022-06-13 13:36:20 -0700

    prints half a string and newline

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of a string
+ * @s: string to print
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int j, i = 0;
+
+	while (*(str + i))
+		i++;
+	j = i / 2;
+	if (i % 2)
+		j += 1;
+	while (j < i)
+	{
+		_putchar(*(str + j));
+		j++;
+	}
+	_putchar('\n');
+}
