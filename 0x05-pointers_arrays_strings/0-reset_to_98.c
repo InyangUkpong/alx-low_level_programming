a5cc227015bc6929aa1b92b9ef92a9efba8510d1
InyangUkpong <inyangweb@gmail.com>
2022-06-13 12:39:30 -0700

    0 task-98 edit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * reset_t0_98 - takes pinter to an int parameter and updates the value it points to 98
+ *
+ * reset_to_98 - takes pinter to an int parameter and updates the value it points to 98
  * @n: int parameter
  *
  * Return: nothing
