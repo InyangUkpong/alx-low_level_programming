65f9e7422d0328738854cf84a438fbda4eb72fce
InyangUkpong <inyangweb@gmail.com>
2022-06-13 13:51:33 -0700

    copies string pointed by src and terminating null to buffer pointed to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy string
+ *
+ * @src: string src
+ * @dest: string dest
+ *
+ * Return: the pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	
+	return (dest);
+}
