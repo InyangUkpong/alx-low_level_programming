5f058cc2acdd92fbc9a6d9ff6b53bbe39972b45d
InyangUkpong <inyangweb@gmail.com>
2022-06-15 16:51:34 -0700

    edited 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,17 +12,17 @@ char *rot13(char *q)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFCHIJKLM";
 
-	while (*(q + count) != '\0')
+while (*(q + count) != '\0')
+{
+	for (i = 0; i < 52; i++)
 	{
-		for (i = 0; i < 52; i++)
-		{
-			if (*(q + count) == alphabet[i])
-			{
-				*(q + count) = rot13[i];
+		if (*(q + count) == alphabet[i])
+	{
+			*(q + count) = rot13[i];
 				break;
-			}
-		}
-		count++;
+	}
+	}
+	count++;
 	}
 
 	return (q);
