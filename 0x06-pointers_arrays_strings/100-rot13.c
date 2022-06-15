f742ec163910cd05baf2b863355fabee9690e154
InyangUkpong <inyangweb@gmail.com>
2022-06-15 16:38:46 -0700

    encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - ecnodes a string using rot13
+ * @q: input string
+ * Return: the pointer to dest
+ */
+
+char *rot13(char *q)
+{
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFCHIJKLM";
+
+	while (*(q + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(q + count) == alphabet[i])
+			{
+				*(q + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (q);
+}
+
