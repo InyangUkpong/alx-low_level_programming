577c67d52fbd7de154e90a8ad2b24703b66efdff
InyangUkpong <inyangweb@gmail.com>
2022-06-15 12:37:27 -0700

    1-strncat -edit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,12 @@
 char *strncat(char *dest, char *src, int n)
 {
 	int dest_len, i;
+
 	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++);
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
+
 	/*should end with an end of string char */
 	dest[dest_len + i] = '\0';
 
