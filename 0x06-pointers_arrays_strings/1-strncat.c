cb38c8f7136176e1fb999417768ebfd84ca3b38f
InyangUkpong <inyangweb@gmail.com>
2022-06-15 12:56:30 -0700

    edited 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,20 @@
 
 char *strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
+	int count = 0, count2 = 0;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++);
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-
-	/*should end with an end of string char */
-	dest[dest_len + i] = '\0';
-
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
