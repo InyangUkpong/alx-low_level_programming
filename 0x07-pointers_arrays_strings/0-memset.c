5c03e4f6c79e0d49bfcfb5ac130f43b45fce98ad
InyangUkpong <inyangweb@gmail.com>
2022-06-20 05:58:15 -0700

    0-memset.c-1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,10 +7,13 @@
  * @n: bytes of memory area to fill
  */
 char *_memset(char *s, char b, unsigned int n);
-	unsigned int a;
-	for (a = 0; a < n; a++;)
 {
+	unsigned int a;
+
+	for (a = 0; a < n; a++;)
+	{
 	s[a] = b;
-}
+	}
 
 return (s);
+}
