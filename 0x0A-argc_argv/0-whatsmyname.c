fd8f1c483004e2569df7326dd02d1ba4ca39caf5
InyangUkpong <inyangweb@gmail.com>
2022-06-24 07:19:15 -0700

    task 0: print name followed by newline

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: counts the command line arguments
+ * @argv: array of stings
+ * Return: 0 when successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
