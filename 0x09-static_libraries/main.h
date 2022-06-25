d3b3de13118dfcd12c8983c0621e4d03c039d878
Queenie Ukpong <83491894+queenieukpong@users.noreply.github.com>
2022-06-25 04:30:53 +0100

    edited main

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-
-/* a list that was given from task 0 */
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif /*"MAIN_H*/
