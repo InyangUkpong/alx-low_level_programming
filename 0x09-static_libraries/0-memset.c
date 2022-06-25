3ffc9b6c37b9083bb4db8cc2ab3c52c85c1b782e
InyangUkpong <inyangweb@gmail.com>
2022-06-24 17:22:39 -0700

    filles memory with a constant byte

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills the memory with constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: by tes filled
+ * Return: the pointer to dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	returns (s);
+}
