b9f31679ff4069679a4f90356687ed1afaac936f
InyangUkpong <inyangweb@gmail.com>
2022-06-24 17:17:43 -0700

    checks for uppercase character

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Retirm" 1 of c is uppercase, else return 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
