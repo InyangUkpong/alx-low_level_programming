ec6ec3b35246e708a11bb261a46418de63da122b
Queenie Ukpong <83491894+queenieukpong@users.noreply.github.com>
2022-06-25 05:01:40 +0100

    4-isalpha edit

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /*
  * File: 4-isalpha.c
- * Auth: Adeyemi Anifowose
+ * Auth: Inyang Ukpong
  */
 
 /**
@@ -14,7 +14,7 @@
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') ||
-	    (c >= 'A' && c <= 'Z'))
+		(c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
