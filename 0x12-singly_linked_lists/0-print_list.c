95b88bf94939247f7a1370b3033523ec098abf0f
InyangUkpong <inyangweb@gmail.com>
2022-07-14 09:55:42 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,27 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
 /**
- * print_list - prints all the elements of a linked list
- * @h: linked list of type list_t to print
+ * print_list - print the elements of a linked list.
+ * @h: liked list head.
  *
- * Return: number of nodes
+ * Return: amount of nodes of the linked list.
+ *
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t num = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		num++;
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		count++;
 	}
-
-	return (num);
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,34 +1,26 @@
-#ifndef LISTS_H
-#define LISTS_H
-
+#ifndef _LIST_
+#define _LIST_
 /**
  * struct list_s - singly linked list
- * @n: integer
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for My ALX project
+ * for Holberton project
  */
 typedef struct list_s
 {
-	int n;
+	char *str;
+	unsigned int len;
 	struct list_s *next;
 } list_t;
-
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const int n);
-list_t *add_node_end(list_t **head, const int n);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void free_list2(list_t **head);
-int pop_list(list_t **head);
-list_t *get_node_at_index(list_t *head, unsigned int index);
-int sum_list(list_t *head);
-list_t *insert_node_at_index(list_t **head, unsigned int idx, int n);
-int delete_node_at_index(list_t **head, unsigned int index);
-list_t *reverse_list(list_t **head);
-size_t print_list_safe(const list_t *head);
-size_t free_list_safe(listint_t **h);
-list_t *find_list_loop(list_t *head);
-
+int _strlen(const char *s);
+void before_main(void)__attribute__((constructor));
 #endif
